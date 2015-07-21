64dc51160c677b1ea7ee87c932cdb8d6e49c16aa
nobody1986 <as_star@qq.com>
2015-07-21 22:57:42 +0800

    .

--- ustring.h
+++ ustring.h
@@ -5,5 +5,11 @@ typedef struct ustring_struct
     int length;
 } ustring;
 
+typedef struct uslice_struct
+{
+    ustring** p;
+    int size;
+} uslice;
 
 ustring* ustring_new(char *str);
+uslice* ustring_split(ustring* str,char *s);
